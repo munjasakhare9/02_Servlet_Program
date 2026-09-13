0b0a80907bb2dee7227f6b210e3d14c066e868c2
Munja Sakhare <154612218+munjasakhare9@users.noreply.github.com>
2026-09-13 11:10:50 +0530

    new string added

--- src/main/java/com/demo/Servlet/First_Servlet.java
+++ src/main/java/com/demo/Servlet/First_Servlet.java
@@ -16,7 +16,7 @@ public class First_Servlet extends HttpServlet {
 		
 		PrintWriter writer=resp.getWriter();
 		writer.append("<h1>Welcome to web development</h1>");
-		String str="I java developer";
+		String str="I am java developer";
 	}
 
 }
