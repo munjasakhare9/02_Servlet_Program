b185fd45d148bcb37cdb1c82d44475d750a51bfe
Munja Sakhare <154612218+munjasakhare9@users.noreply.github.com>
2026-09-13 10:42:37 +0530

    new var added

--- src/main/java/com/demo/Servlet/First_Servlet.java
+++ src/main/java/com/demo/Servlet/First_Servlet.java
@@ -16,6 +16,8 @@ public class First_Servlet extends HttpServlet {
 		
 		PrintWriter writer=resp.getWriter();
 		writer.append("<h1>Welcome to web development</h1>");
+		
+		String name = "java";
 	}
 
 }
